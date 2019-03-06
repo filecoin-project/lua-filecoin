47e3beadf84a456d74dd31ffa12476bfb2231914
Tim Caswell <tim@creationix.com>
2019-03-05 19:01:24 -0600

    Add lit app shell

---
+++ package.lua
@@ -0,0 +1,21 @@
+  return {
+    name = "filecoin-project/lua-filecoin",
+    version = "0.0.1",
+    description = "Prototype of Filecoin in Lua",
+    tags = { "filecoin", "p2p" },
+    luvi = {
+        flavor = "regular"
+    },
+    license = "MIT/Apache",
+    author = { name = "Tim Caswell", email = "tim.caswell@protocol.ai" },
+    homepage = "https://github.com/filecoin-project/lua-filecoin",
+    dependencies = {
+        "luvit/require",
+	"luvit/pretty-print",
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
